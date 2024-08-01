6ba59f447b760bb6872c7bc0d905754adc99d321
lukby2457 <kjh7442455@gmail.com>
2024-08-01 11:40:41 +0900

    3번 문제 2번 답 재수정 완료

--- 03.js
+++ 03.js
@@ -10,22 +10,22 @@ const obj1 = {
 console.log(obj1.getValue()) ; //  출력값 20
 
 // 2. 웹 브라우저에서 setTimeout 함수 내에서 this가 가리키는 출력값이 왜 그런지 설명해주세요
-const obj2 = {
-    method : function() {
-        console.log('첫번째 this : ', this);
+  const obj2 = {
+      method : function() {
+          console.log('첫번째 this : ', this);
 
-        const innerMethod = () => {
-            setTimeout(function() {
-                console.log('두번째 this : ' , this); 
-            });
-        }
-        innerMethod();
-    }
-}
+          const innerMethod = () => {
+              setTimeout(function() {
+                  console.log('두번째 this : ' , this); 
+              });
+          }
+          innerMethod();
+      }
+  }
 
-obj2.method();
+  obj2.method();
 
 /*
-  화살표 함수는 this를 바인딩하지 않으나 setTimeout 함수 내에 function으로 일반 함수로 setTimeout의 인자로 사용하고 있다.
-  따라서, 화살표 함수의 this는 setTimeout을 출력하게 된다
+  화살표 함수는 this를 바인딩하지 않는다. 그래서 웹 브라우저에서 출력하면 전역 객체인 Window가 나오게 된다.
+  따라서 this는 Window가 출력되게 된다.
 */
